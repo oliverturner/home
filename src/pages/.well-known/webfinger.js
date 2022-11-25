0d876b657ec528ed8c90f0224f1c03616dcc77a7
Oliver Turner <oliverturner@users.noreply.github.com>
2022-11-25 16:31:20 +0000

    Add RSS link and webfinger

---
+++ src/pages/.well-known/webfinger.js
@@ -0,0 +1,27 @@
+export async function get() {
+  return {
+    body: JSON.stringify({
+      subject: "acct:oliverturner@toot.cafe",
+      aliases: [
+        "https://toot.cafe/@oliverturner",
+        "https://toot.cafe/users/oliverturner",
+      ],
+      links: [
+        {
+          rel: "http://webfinger.net/rel/profile-page",
+          type: "text/html",
+          href: "https://toot.cafe/@oliverturner",
+        },
+        {
+          rel: "self",
+          type: "application/activity+json",
+          href: "https://toot.cafe/users/oliverturner",
+        },
+        {
+          rel: "http://ostatus.org/schema/1.0/subscribe",
+          template: "https://toot.cafe/authorize_interaction?uri={uri}",
+        },
+      ],
+    }),
+  };
+}
