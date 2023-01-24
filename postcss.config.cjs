627647e26f22385e34c530f90437442f67fb41fd
Oliver Turner <oliverturner@users.noreply.github.com>
2023-01-24 19:33:57 +0000

    Update all deps & remove redundant plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,19 +1,7 @@
 async function getConfig() {
-	const { default: size } = await import("./tokens/size.cjs");
-
-	const customMedia = {};
-	for (const [k, v] of Object.entries(size.viewport)) {
-		const n = parseInt(v.value, 10);
-		customMedia[`--mq-${k}`] = `(min-width: ${n}ch)`;
-		customMedia[`--mq-max-${k}`] = `(max-width: ${n}ch)`;
-	}
-
-	console.log({ customMedia });
-
 	return {
 		plugins: {
 			"@csstools/postcss-design-tokens": { valueFunctionName: "dt" },
-			"postcss-custom-media": { importFrom: { customMedia } },
 			"postcss-nesting": {},
 		},
 	};
